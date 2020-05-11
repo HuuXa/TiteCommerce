e7afdbf0ab8a6490df03f3e346666b091e807820
HuuXa <thxa.12b8.pdl@gmail.com>
2020-05-11 21:52:55 +0700

    first code

---
+++ LiteCommerce.Admin/Controllers/CatulogShippersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiteCommerce.Admin.Controllers
+{
+    public class CatulogShippersController : Controller
+    {
+        // GET: CatulogShippers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LiteCommerce.Admin/Controllers/OralersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiteCommerce.Admin.Controllers
+{
+    public class OralersController : Controller
+    {
+        // GET: Oralers
+        public ActionResult list()
+        {
+            return View();
+        }
+        public ActionResult creatr()
+        {
+            return View();
+        }
+    }
+}
